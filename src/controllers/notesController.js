98b199e319defd2d7efdb0502fdb0415ba1b4873
bohdan_berest <berestbodi@gmail.com>
2026-02-28 17:44:03 +0200

    fix 404 error

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,18 +1,17 @@
-import createError from 'http-errors';
-import Note from '../models/note.js';
 import createHttpError from 'http-errors';
+import Note from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
 
-export const getNoteById = async (req, res, next) => {
+export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findById(noteId);
 
   if (!note) {
-    throw createHttpError(createError(404, 'Note not found'));
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
@@ -23,23 +22,23 @@ export const createNote = async (req, res) => {
   res.status(201).json(note);
 };
 
-export const deleteNote = async (req, res, next) => {
+export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findByIdAndDelete(noteId);
 
   if (!note) {
-    throw createHttpError(createError(404, 'Note not found'));
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
 };
 
-export const updateNote = async (req, res, next) => {
+export const updateNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findByIdAndUpdate(noteId, req.body, { new: true });
 
   if (!note) {
-    throw createHttpError(createError(404, 'Note not found'));
+    throw createHttpError(404, 'Note not found');
   }
 
   res.status(200).json(note);
