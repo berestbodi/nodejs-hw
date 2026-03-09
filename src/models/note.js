ce6fa4c192d7b52819d12d1a9921c058e104b083
bohdan_berest <berestbodi@gmail.com>
2026-03-09 09:23:04 +0200

    fix models

--- src/models/note.js
+++ src/models/note.js
@@ -1,15 +1,29 @@
 import { Schema, model } from 'mongoose';
-import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    tag: { type: String, enum: TAGS, required: true },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      default: 'Todo',
+      required: false,
+    },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
 );
 
-noteSchema.index({ title: 'text', content: 'text' });
+const Note = model('note', noteSchema);
 
-export const Note = model('note', noteSchema);
+export default Note;
